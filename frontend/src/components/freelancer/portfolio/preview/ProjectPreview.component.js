95f54a75d699ada7b2b4def20e674344ca42454e
abdelfetah18 <nstart0319@gmail.com>
2023-11-23 16:24:02 +0100

    Project preview UI

---
+++ frontend/src/components/freelancer/portfolio/preview/ProjectPreview.component.js
@@ -0,0 +1,9 @@
+import { FreeLancerHeaderVue } from '@/components/freelancer/header';
+import { SliderVue } from "@/components/slider";
+
+export default {
+    components: { FreeLancerHeaderVue, SliderVue },
+    setup(){
+        return {};
+    }
+}
