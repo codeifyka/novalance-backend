e062e20d708ef6d08195c75839c924b5a71ed774
unknown <kadiksalah03@gmail.com>
2024-08-02 17:07:10 +0100

    get all freelancers in client chat

--- frontend/src/components/client/chat/Chat.component.js
+++ frontend/src/components/client/chat/Chat.component.js
@@ -1,11 +1,35 @@
 import { ClientHeaderVue } from '@/components/client/header';
-import { ClientFooterVue } from '@/components/client/footer';
+import { ref, onMounted, inject, nextTick } from 'vue';
+import RestChat from '@/libs/RestChat';
+import { ClientChatRoomVue } from './chat_room';
 
 export default {
-  components: { ClientHeaderVue , ClientFooterVue  },
-  setup() {   
-    return {
+  components: { ClientHeaderVue, ClientChatRoomVue },
+  setup() {
+    const axios = inject('axios');
+    const restChat = new RestChat(axios)
+    const freelancers = ref(null)
     
+    const getFreelancer = async () => {
+      try{
+        const response = await restChat.getFreelancers()
+        console.log("first")
+        console.log(response)
+      if(response.data){
+        freelancers.value = response.data
+        
+      }
+    }catch(err){
+      console.log(err)
+    }
+    }
+    
+    onMounted(() => {
+      getFreelancer()
+    });
+
+    return {
+      freelancers
     };
   },
 };
