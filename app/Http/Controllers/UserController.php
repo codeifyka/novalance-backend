df8d3f8d4fc983745227878b74cc9743d833caf2
Salah Eddine Kadik <123268700+kadik23@users.noreply.github.com>
2025-01-31 10:45:09 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,7 +17,11 @@ class UserController extends Controller
                     "username" => auth('api')->user()->username,
                     "first_name" => auth('api')->user()->first_name,
                     "last_name" => auth('api')->user()->last_name,
-                    "email" => auth('api')->user()->email
+                    "email" => auth('api')->user()->email,
+                    "account_type" => auth('api')->user()->account_type,
+                    "country" => auth('api')->user()->country,
+                    "gender" => auth('api')->user()->gender,
+                    "telephone" => auth('api')->user()->telephone,
                 ],
                 "services" => auth('api')->user()->services->count(),
                 "projects" => auth('api')->user()->projects->count(),
@@ -28,16 +32,56 @@ class UserController extends Controller
     public function updateInfo(Request $request)
     {        
         $users = User::query()->where('username','=',$request['username'])->get();
-        if(count($users) > 0 && $users[0]->username != auth('api')->user()->username){
+        if(count($users) > 0 && $request['username'] != auth('api')->user()->username){
             return response()->json([
                 "error" => "Username already in use"
+            ]);
+        }
+
+        if(count($users) > 0 && $request['first_name'] != auth('api')->user()->first_name){
+            return response()->json([
+                "error" => "First Name already in use"
+            ]);
+        }
+        
+        if(count($users) > 0 && $request['last_name'] != auth('api')->user()->last_name){
+            return response()->json([
+                "error" => "Last Name already in use"
+            ]);
+        }
+
+        if(count($users) > 0 && $request['country'] != auth('api')->user()->country){
+            return response()->json([
+                "error" => "Country Name already in use"
+            ]);
+        }
+
+        if(count($users) > 0 && $request['telephone'] != auth('api')->user()->telephone){
+            return response()->json([
+                "error" => "Telephone Name already in use"
+            ]);
+        }
+
+        if(count($users) > 0 && $request['gender'] != auth('api')->user()->gender){
+            return response()->json([
+                "error" => "Gender Name already in use"
+            ]);
+        }
+
+        if(count($users) > 0 && $request['email'] != auth('api')->user()->email){
+            return response()->json(data: [
+                "error" => "Email Name already in use"
             ]);
         }
         
         User::query()->where('id','=', auth('api')->user()->id)->update([
             'username' => $request['username'],
             'first_name' => $request['first_name'],
-            'last_name' => $request['last_name']
+            'last_name' => $request['last_name'],
+            'email' => $request['email'],
+            'gender' => $request['gender'],
+            'telephone' => $request['telephone'],
+            'country' => $request['country'],
         ]);
         
         return response()->json([
